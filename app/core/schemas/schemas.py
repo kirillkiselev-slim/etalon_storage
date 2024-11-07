1360f0469f8d7e299dd7c0e62cb81274706ec557
kirillkiselev-slim <kiselev99kirill1501@gmail.com>
2024-11-07 21:55:15 +0300

    add more schemas

--- app/core/schemas/schemas.py
+++ app/core/schemas/schemas.py
@@ -3,16 +3,27 @@ from decimal import Decimal
 from typing import Dict, Annotated
 
 from pydantic import BaseModel, fields, conint, ConfigDict
+from app.core.constants import (PRODUCTION_BATCHES_REGEX,
+                                PRODUCTION_BATCHES_DESCRIPTION_STATUS,
+                                PRODUCT_DESCRIPTION_STATUS, PRODUCT_REGEX,
+                                SHIPMENTS_REGEX, SHIPMENTS_DESCRIPTION_STATUS)
 
 
 class BaseConfigModel(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
 
-class Product(BaseConfigModel):
+class ProductGet(BaseConfigModel):
     id: int
     model: str
     status: str
+
+
+class ProductCreate(BaseConfigModel):
+    model: str = fields.Field(max_length=255, min_length=3)
+    status: str = fields.Field(min_length=5, default='IN_STOCK',
+                               pattern=PRODUCT_REGEX,
+                               description=PRODUCT_DESCRIPTION_STATUS)
 
 
 class Warehouse(BaseConfigModel):
